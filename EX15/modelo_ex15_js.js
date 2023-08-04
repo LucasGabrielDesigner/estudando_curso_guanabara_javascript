1c416d7b1415ad7eba2ad8efbcffed3c4e765588
Lucas Gabriel <contato@lucasgabrieldesigner.com>
2023-08-04 20:51:13 -0300

    Update modelo_ex15_js.js

--- EX15/modelo_ex15_js.js
+++ EX15/modelo_ex15_js.js
@@ -16,26 +16,35 @@ function verificar(){
         if(fsex[0].checked){
             genero = "Homem"
             if(idade < 10){
-                //Criança 
+                //Criança
+                img.setAttribute("src", "criança_homem.png") 
             }else if(idade < 18){
                 //Jovem
+                img.setAttribute("src", "adolescente_homem.png")
             }else if(idade < 65){
                 //Adulto
+                img.setAttribute("src", "adulto_homem.png")
             }else{
                 //Olavo de Carvalho
-            }
+                img.setAttribute("src", "Olavo.png")
+            } 
             
-        }else if(fsex[1].checked){
+        } if(fsex[1].checked){
             genero = "Mulher"
-           
-        }
-                
-            
-        
+            if(idade < 10){
+                //Criança
+                img.setAttribute("src", "criança_mulher.png") 
+            }else if(idade < 18){
+                //Jovem
+                img.setAttribute("src", "adolescente_mulher.png")
+            }else if(idade < 65){
+                //Adulto
+                img.setAttribute("src", "adulto_mulher.png")
+            }else{
+                //Olavo de Carvalho
+                img.setAttribute("src", "Olavo.png")}
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         res.style.textAlign = "center"
+        res.appendChild(img)
     }
-
-
-}
-
+}}
