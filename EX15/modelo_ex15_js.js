c58c936474d27e9a94e0aa4be904dd5a3ea8688e
Lucas Gabriel <contato@lucasgabrieldesigner.com>
2023-08-04 18:04:22 -0300

    .

--- EX15/modelo_ex15_js.js
+++ EX15/modelo_ex15_js.js
@@ -1,3 +1,13 @@
 function verificar(){
-    window.alert("Funcionou")
+    var data = new Date()
+    var ano = data.getFullYear
+    var fano = document.getElementById("txtano")
+    var res = document.querySelector("div#res")
+
+    if(fano.value.length == 0 || fano.value > ano){
+        alert("[ERRO] Verifique os dados ai.Tenta ai de novo fera!")
+    } else{
+        alert("Tudo certo manolo!")
+    }
+
 }
